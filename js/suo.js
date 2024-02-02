19f96d8ff9283d097030bd2010220ed138469599
小良 <84088289@qq.com>
2024-02-02 15:10:31 +0800

    新增ai对话功能

--- js/suo.js
+++ js/suo.js
@@ -8,5 +8,19 @@ $(document).ready(function() {
         $(".on").removeClass("on"),
         $(".list").addClass("closed")
 
-    })
+    }),
+    $('#ai-icon').click(function(s){
+        s.stopPropagation();
+        $(".ai-content").toggle();
+    }),
+    $('.ai-content').click(function(event) {  
+        event.stopPropagation(); // 阻止事件冒泡到body元素  
+      });  
+      $(document).click(function(event) {  
+        var myDiv = $('.ai-content');  
+        if (!myDiv.is(event.target) && myDiv.has(event.target).length === 0) { // 检查事件目标是否是div的子元素或div本身  
+          myDiv.hide(); // 隐藏div  
+        // myDiv.toggle()
+        }  
+      });  
 });
